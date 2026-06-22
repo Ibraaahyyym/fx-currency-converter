a64e17f72e1949b065429971061ccd04bd75518a
Ibrahim Garba <usrx.random@gmail.com>
2026-06-22 17:47:44 +0100

    added the frameworkk for auto scrolling market data

---
+++ js/main.js
@@ -0,0 +1,46 @@
+
+//const liveMarketData = document.getElementById("live-market-data");
+
+// fetch live msrwket data from Frankfurter API and append it to the DOM
+fetch("https://api.frankfurter.dev/v2/rates?base=USD")
+  .then(res=>res.json())
+  .then(data=>{
+    //console.log(data);
+    // loop through thr array safely
+    for(let i=0; i<data.length; i++){
+        // create a list item for each market data and append it to the DOM
+        let marketData = document.createElement("li");
+        marketData.textContent = `${data[i].base}/${data[i].quote} ${data[i].rate}`;
+        
+        liveMarketData.appendChild(marketData);
+    }
+  })
+  .catch(err=>{
+    console.error("error: ",err)
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
